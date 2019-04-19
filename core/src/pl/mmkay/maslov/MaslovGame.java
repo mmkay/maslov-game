c749ffb891eec130afd928e540f29f68afe19733
Mateusz Kulewicz <mateusz.kulewicz@gmail.com>
2019-04-19 20:24:20 +0200

    Initial commmit - auto-generated project

---
+++ core/src/pl/mmkay/maslov/MaslovGame.java
@@ -0,0 +1,31 @@
+package pl.mmkay.maslov;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class MaslovGame extends ApplicationAdapter {
+	SpriteBatch batch;
+	Texture img;
+	
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+	}
+
+	@Override
+	public void render () {
+		Gdx.gl.glClearColor(0.376f, 0.502f, 0.22f, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		batch.begin();
+		batch.end();
+	}
+	
+	@Override
+	public void dispose () {
+		batch.dispose();
+		img.dispose();
+	}
+}
---
+++ desktop/src/pl/mmkay/maslov/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package pl.mmkay.maslov.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import pl.mmkay.maslov.MaslovGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MaslovGame(), config);
+	}
+}
